44266fc29361c585aaf4e0f694f34492adb346ac
Jacob Perron <jacob@openrobotics.org>
2020-07-28 13:54:43 -0700

    Move script to 'scripts' directory
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/' + package_name + '/resource', ['resource/RosGraph.ui']),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['plugin.xml']),
-        ('lib/' + package_name, ['bin/rqt_graph']),
+        ('lib/' + package_name, ['scripts/rqt_graph']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
