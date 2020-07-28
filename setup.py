4219ab552cec4243aab241548b5084eab1750811
Jacob Perron <jacob@openrobotics.org>
2020-07-27 17:16:50 -0700

    Install exectuable rqt_graph
    This fixes a regression introduced in #42.
    
    Now the application can be started with 'rqt_graph' or with 'ros2 run'
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/' + package_name + '/resource', ['resource/RosGraph.ui']),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['plugin.xml']),
+        ('bin', ['bin/rqt_graph']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
