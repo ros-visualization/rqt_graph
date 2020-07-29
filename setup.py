ros-visualization/rqt_graph

commit 4139a3957acc0b3e2e8258cca9ddb46f62258190

Add setup.cfg with script install directories (#42)
This fixes an issue running rqt_graph with ros2run on Windows.
ros2run expects scripts to be in the lib directory.

Signed-off-by: ahcorde <ahcorde@gmail.com>

setup.cfg
@@ -1,0 +1,4 @@
+[develop]
+script-dir=$base/lib/rqt_graph
+[install]
+install-scripts=$base/lib/rqt_graph

commit 9bce79ca3ed938551bae5418a9f65a71a01c5bf5

Install executable rqt_graph (#49)
* Install exectuable rqt_graph

This fixes a regression introduced in #42.

Now the application can be started with 'rqt_graph' or with 'ros2 run'

Signed-off-by: Jacob Perron <jacob@openrobotics.org>

* Revert "Add setup.cfg with script install directories (#42)"

This reverts commit 4139a3957acc0b3e2e8258cca9ddb46f62258190.

* Install entrypoint to bin/ and executable to lib/rqt_graph/

Signed-off-by: Jacob Perron <jacob@openrobotics.org>

* Move script to 'scripts' directory

Signed-off-by: Jacob Perron <jacob@openrobotics.org>

scripts/rqt_graph
@@ -1,0 +1,7 @@
+#!/usr/bin/env python3
+
+import sys
+
+from rqt_graph.main import main
+
+sys.exit(main())

setup.cfg
@@ -1,4 +1,0 @@
-[develop]
-script-dir=$base/lib/rqt_graph
-[install]
-install-scripts=$base/lib/rqt_graph

setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/' + package_name + '/resource', ['resource/RosGraph.ui']),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['plugin.xml']),
+        ('lib/' + package_name, ['scripts/rqt_graph']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,