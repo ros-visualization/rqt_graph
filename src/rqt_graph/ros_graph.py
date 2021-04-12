778d68a886b4c7f4a31b60e487e46a7d0d7caaa8
skudryas <skudryas@skudryas.ru>
2021-04-12 20:04:36 +0300

    graph load/save into DOT file corrections for py3
    (cherry picked from commit 0f24f498bbfd58854b1d46baff85e246f0782188)

--- src/rqt_graph/ros_graph.py
+++ src/rqt_graph/ros_graph.py
@@ -398,7 +398,7 @@ class RosGraph(Plugin):
         self._widget.group_image_check_box.setEnabled(False)
         self._widget.hide_dynamic_reconfigure_check_box.setEnabled(False)
 
-        self._update_graph_view(dotcode)
+        self._update_graph_view(dotcode.decode('utf-8'))
 
     def _fit_in_view(self):
         self._widget.graphics_view.fitInView(self._scene.itemsBoundingRect(), Qt.KeepAspectRatio)
@@ -413,7 +413,7 @@ class RosGraph(Plugin):
         if not handle.open(QIODevice.WriteOnly | QIODevice.Text):
             return
 
-        handle.write(self._current_dotcode)
+        handle.write(self._current_dotcode.encode())
         handle.close()
 
     def _save_svg(self):
