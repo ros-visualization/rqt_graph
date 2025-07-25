2e7b7e16152649e37d619632a600c4b18bbaf227
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-07-25 12:35:45 +0200

    1.5.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt_graph'
 setup(
     name=package_name,
-    version='1.5.4',
+    version='1.5.5',
     package_dir={'': 'src'},
     packages=[package_name],
     data_files=[
