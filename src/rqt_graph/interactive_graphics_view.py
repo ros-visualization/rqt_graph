7253f225f909280990a741bf8ba876269ad00201
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-06-15 11:30:22 +0200

    Removed Python2 references and Qt6 fixes
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- src/rqt_graph/interactive_graphics_view.py
+++ src/rqt_graph/interactive_graphics_view.py
@@ -28,8 +28,6 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from __future__ import division
-
 from python_qt_binding.QtCore import QPointF, QRectF, Qt
 from python_qt_binding.QtGui import QTransform
 from python_qt_binding.QtWidgets import QGraphicsView
